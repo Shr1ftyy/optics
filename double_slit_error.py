6428ac5eeacae50d48ae84261790af616fb0ac6d
Shr1ftyy <49330057+Shr1ftyy@users.noreply.github.com>
2021-08-13 19:21:54 +0930

    updated - corrected equation to compare against

--- double_slit_error.py
+++ double_slit_error.py
@@ -5,14 +5,14 @@ from mpl_toolkits.mplot3d import Axes3D
 
 d = (1/600)*(10**(-3))
 
-# value of mu is hardcoded for now
-def error(delta_y, L):
-  # return (mu)*(alpha**2) - 2*(alpha)*(x**2) -2*(mu)*(x**2) + 4*(x**2)
-  return 100*(abs(d*math.sin(math.atan(delta_y/L)) - ((d*delta_y)/L)))/abs(d*math.sin(math.atan(delta_y/L)))
+def error(y, L):
+  r_1 = math.sqrt((y-(d/2))**2+L**2)
+  r_2 = math.sqrt((y+(d/2))**2+L**2)
+  phi = r_2 - r_1
+  return abs( phi - (d*y/L) / (phi)) * 100
 
-L_s = np.linspace(5, 110.0, 100)
-# 0<x<(alpha/2)
-delta_y_s = np.linspace(5, 110.0, 100)
+L_s = np.linspace(0.1, 2, 100)
+delta_y_s = np.linspace(0.1, 2, 100)
 
 
 def generate(x_s, alphas, func):
@@ -25,27 +25,22 @@ def generate(x_s, alphas, func):
 
 
 Y, X = np.meshgrid(delta_y_s, L_s)
-# Values for volume 
+
 errors = generate(delta_y_s, L_s, error)
-# Values for derivative of volume 
+
 
 fig = plt.figure()
 
 ax = fig.add_subplot(111, projection='3d') 
-ax.set_zlim3d(-10,110)
-# ax.set_title('Error')
+# ax.set_zlim3d(-1,100)
+# plt.rcParams['axes.titlepad'] = 40
+ax.set_title('Wavelength Value Error Due to Angle Approximation – Double Slit')
 ax.set_xlabel('Δy (m)')
 ax.set_ylabel('L (m)')
 ax.set_zlabel('Error %')
 ax.plot_surface(X, Y, errors, color='y', rstride=4, cstride=4, linewidth=1, alpha=0.75, edgecolors='blue')
 
 
-# ax = fig.add_subplot(121, projection='3d') 
-# ax.set_title('Derivative of Volume Function')
-# ax.set_xlabel('x')
-# ax.set_ylabel('α')
-# ax.set_zlabel("error'(x)")
-# ax.plot_surface(X, Y, errors, color='b')
 
 plt.draw()
 plt.show()
